0f1109c05761a4947f33ceb78397b4a07066c87d
Stefanomarelli <117829913+5590355@users.noreply.github.com>
2022-12-17 00:00:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+int  fun ( char lista[ 10 ][ 20 ], char nome[ 20 ]){
+  intero io = 0 ;
+  intero j = 0 ;
+  mentre ((j < 19 ) e (i < 10 )) {
+    if (lista[i][j] != nome[j]) {
+      io++;
+      j = 0 ;
+    } else  if (lista[i][j] == nome[j]) {
+      j++;
+    }
+  } restituisce i;
+}
