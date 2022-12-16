15c5dd1644a43594763fc2abdf8e0936ef018e14
Stefanomarelli <117829913+5590355@users.noreply.github.com>
2022-12-17 00:30:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,33 @@
-#include <iostream>
-#include "lib.h"
-usando lo spazio dei nomi std;
+# include  < iostream >
+# include  " lib.h "
+usando lo  spazio dei nomi  std ;
 
-int principale(){
-  char lista [10][20];
-  char nome [20];
-  for (int x=0; x<10; x++) {
-    for(int y=0; y<20; y++){
-      lista [x][y]= '  ';
+int  principale () {
+    char lista[ 10 ][ 20 ];
+    destinazione char [ 20 ];
+    int posizione = 0 ;
+
+    for ( int i= 0 ;i< 10 ;i++){
+        for ( int j= 0 ;j< 20 ;j++) {
+            destinazione[j] = '  ' ;
+            lista[i][j] = '  ' ;
+        }
     }
-  }
-  for (int s=0; s<20; s++){
-    nome[s]= '  ';
-  }
-  for (int x=0; x<10; x++){
-    cin>>lista[x];
-  }
-  for (int x=0; x<10; x++){
-    cout<<lista[x]<<endl;
-  }
-  cin>>nome;
-  int x=divertimento (lista,nome);
-  se (x<10){
-    cout<<x;
-  } altrimenti se (x==10){
-    cout<<"non presente";
-  }
-  ritorno 0;
+
+    for ( int i= 0 ;i< 10 ;i++){
+        cin >> lista[i];
+    }
+
+    cin >> bersaglio;
+    if ( cerca (lista,pos,target)){
+        cout << pos;
+    } altro {
+        cout << " non presente " ;
+    }
+
+
+
+
+
+    ritorno  0 ;
 }
